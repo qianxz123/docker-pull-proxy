e12f7e2953d1f928218896f724c4fbe604ea9061
qianxz123 <133915315+qianxz123@users.noreply.github.com>
2024-06-15 21:14:57 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -5,21 +5,22 @@
 # "Architecture": "arm64",
 
 # 不指定 cpu 架构
-cat trigger.txt |awk '{print "docker pull " $1} '
-cat trigger.txt |awk '{print "docker pull " $1} '| sh
+cat trigger.txt | grep -v '#'|awk '{print "docker pull " $1} '
+cat trigger.txt | grep -v '#'|awk '{print "docker pull " $1} '| sh
 
 #指定 cpu 架构
-# cat trigger.txt |awk '{print "docker pull --platform linux/arm64 " $1} '
-# cat trigger.txt |awk '{print "docker pull --platform linux/arm64 " $1} '| sh
+# cat trigger.txt | grep -v '#' |awk '{print "docker pull --platform linux/arm64 " $1} '
+# cat trigger.txt | grep -v '#' |awk '{print "docker pull --platform linux/arm64 " $1} '| sh
 
 # inspect Architectur
-# cat trigger.txt |awk '{print "docker image inspect  " $1 "| grep Architectur" } '
-# cat trigger.txt |awk '{print "docker image inspect  " $1 "| grep Architectur" } '| sh
+# cat trigger.txt | grep -v '#' |awk '{print "docker image inspect  " $1 "| grep Architectur" } '
+# cat trigger.txt | grep -v '#' |awk '{print "docker image inspect  " $1 "| grep Architectur" } '| sh
 
 # docker tag
-cat trigger.txt |awk '{print "docker tag "$1 " " $2} '
-cat trigger.txt |awk '{print "docker tag "$1 " " $2} '| sh
+cat trigger.txt | grep -v '#' |awk '{print "docker tag "$1 " " $2} '
+cat trigger.txt | grep -v '#' |awk '{print "docker tag "$1 " " $2} '| sh
 
 # docker push
-cat trigger.txt |awk '{print "docker push " $2} '
-cat trigger.txt |awk '{print "docker push " $2} '| sh
+cat trigger.txt | grep -v '#' |awk '{print "docker push " $2} '
+cat trigger.txt | grep -v '#' |awk '{print "docker push " $2} '| sh
+
